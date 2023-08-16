dfc811887eb364ec499f8d0f2325952183a17aaa
Andrew Tran <tranandrew@gmail.com>
2023-08-16 13:41:10 -0500

    import lombok, import javax persistence

--- src/main/java/com/diningreview/DiningReviewAPI/model/Restaurant.java
+++ src/main/java/com/diningreview/DiningReviewAPI/model/Restaurant.java
@@ -1,5 +1,29 @@
 package com.diningreview.DiningReviewAPI.model;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
+import lombok.Generated;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity
 public class Restaurant {
+    @Id
+    @GeneratedValue
+    private Long id;
+    String name;
+    String address;
+    Integer phoneNumber;
+    double reviewScore;
+    double peanut;
+    double egg;
+    double dairy;
+
 
 }
+
