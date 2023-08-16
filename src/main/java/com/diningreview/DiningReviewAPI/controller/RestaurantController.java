36902bf95453a8c7f28e50a01b1ca9b5c242bff6
Andrew Tran <tranandrew@gmail.com>
2023-08-16 18:19:57 -0500

    RestaurantController created. Import models, repositories, and springframework annotations. repository fields and constructor setup

--- src/main/java/com/diningreview/DiningReviewAPI/controller/RestaurantController.java
+++ src/main/java/com/diningreview/DiningReviewAPI/controller/RestaurantController.java
@@ -1,4 +1,20 @@
 package com.diningreview.DiningReviewAPI.controller;
 
+import com.diningreview.DiningReviewAPI.model.*;
+import com.diningreview.DiningReviewAPI.repository.*;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@RequestMapping("/diningreview")
 public class RestaurantController {
+    private final RestaurantRepository restaurantRepository;
+    private final UserRepository userRepository;
+    private final ReviewRepository reviewRepository;
+
+    public RestaurantController(RestaurantRepository restaurantRepository, UserRepository userRepository, ReviewRepository reviewRepository) {
+        this.restaurantRepository = restaurantRepository;
+        this.userRepository = userRepository;
+        this.reviewRepository = reviewRepository;
+    }
 }
