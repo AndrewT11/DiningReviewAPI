7907194f80c5a2c87c0d29195ae0efb25db51646
Andrew Tran <tranandrew@gmail.com>
2023-08-16 13:45:50 -0500

    adding user.java

--- src/main/java/com/diningreview/DiningReviewAPI/model/User.java
+++ src/main/java/com/diningreview/DiningReviewAPI/model/User.java
@@ -1,4 +1,24 @@
 package com.diningreview.DiningReviewAPI.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity
 public class User {
+    @Id
+    @GeneratedValue
+    long id;
+    @Column
+    String userName;
+    @Column
+    String rating;
 }
