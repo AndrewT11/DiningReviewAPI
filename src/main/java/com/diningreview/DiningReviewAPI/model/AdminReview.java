592f39d93713567c8cc6c7851e172d6b85ba583e
Andrew Tran <tranandrew@gmail.com>
2023-08-18 12:54:31 -0500

    imports of lombok, annotations written, reviewAccepted field added

--- src/main/java/com/diningreview/DiningReviewAPI/model/AdminReview.java
+++ src/main/java/com/diningreview/DiningReviewAPI/model/AdminReview.java
@@ -1,4 +1,16 @@
 package com.diningreview.DiningReviewAPI.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+// no information is persisted here, so no need to add Spring JPA annotations to persist data.
+
+@AllArgsConstructor
+@Getter
+@Setter
 public class AdminReview {
+    private boolean reviewAccepted;
+
+
 }
