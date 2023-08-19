91bf11cf9aed60e5e5e430cb2b4387cfc4938965
Andrew Tran <tranandrew@gmail.com>
2023-08-18 20:50:11 -0500

    Add updateUser method. Figure out how to add certain pieces of information and not allow others

--- src/main/java/com/diningreview/DiningReviewAPI/controller/UserController.java
+++ src/main/java/com/diningreview/DiningReviewAPI/controller/UserController.java
@@ -3,8 +3,13 @@ package com.diningreview.DiningReviewAPI.controller;
 
 import com.diningreview.DiningReviewAPI.model.User;
 import com.diningreview.DiningReviewAPI.repository.UserRepository;
+
+// Spring controllers lesson
 import org.springframework.web.bind.annotation.*;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
+
+
 
 
 import java.util.Optional; // used to confirm if object isPresent() or not
@@ -20,8 +25,21 @@ public class UserController {
 
     // Create user
     @PostMapping
-    @ResponseStatus(HttpStatus.CREATED)
+    @ResponseStatus(code = HttpStatus.CREATED, reason = "User created")
     public void addUser(@RequestBody User user) {
         userRepository.save(user);
     }
+
+    @PutMapping("/{userName}")
+    @ResponseStatus(code = HttpStatus.RESET_CONTENT, reason = "User updated")
+    public void updateUser(@PathVariable String userName, @RequestBody User updatedInfo) {
+        // Validate user exists
+        Optional<User> existingUserOptional = this.userRepository.findUserByUserName(userName);
+        if(existingUserOptional.isPresent()) {
+            User existingUser = existingUserOptional.get();
+            this.userRepository.save(existingUser);
+        }
+
+    }
+
 }
