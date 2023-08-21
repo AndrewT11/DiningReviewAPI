86d1ffce88c03956faed1bcea62797f8bd0fa697
Andrew Tran <tranandrew@gmail.com>
2023-08-21 12:15:09 -0500

    POJO queries added to ReviewRepository

--- src/main/java/com/diningreview/DiningReviewAPI/repository/ReviewRepository.java
+++ src/main/java/com/diningreview/DiningReviewAPI/repository/ReviewRepository.java
@@ -1,10 +1,13 @@
 package com.diningreview.DiningReviewAPI.repository;
 
 import com.diningreview.DiningReviewAPI.model.Review;
-import java.util.Optional;
+import com.diningreview.DiningReviewAPI.model.ReviewStatus;
+
 import java.util.List;
+
 
 import org.springframework.data.repository.CrudRepository;
 public interface ReviewRepository extends CrudRepository<Review, Long> {
-
+    List<Review> findReviewByRestaurantIdAndStatus(Long restaurantId, ReviewStatus reviewStatus);
+    List<Review> findReviewByStatus(ReviewStatus reviewStatus);
 }
