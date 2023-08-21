1f998bbb2d92ad778f407f189ac9934f14af3469
Andrew Tran <tranandrew@gmail.com>
2023-08-21 12:45:42 -0500

    @RequestMapping and @RestController annotations to ReviewController. Initialized user, review, and restaurant repositories in ReviewController.

--- src/main/java/com/diningreview/DiningReviewAPI/controller/ReviewController.java
+++ src/main/java/com/diningreview/DiningReviewAPI/controller/ReviewController.java
@@ -1,4 +1,5 @@
 package com.diningreview.DiningReviewAPI.controller;
+
 import com.diningreview.DiningReviewAPI.model.Review;
 import com.diningreview.DiningReviewAPI.model.ReviewStatus;
 import com.diningreview.DiningReviewAPI.model.User;
@@ -8,8 +9,23 @@ import com.diningreview.DiningReviewAPI.repository.ReviewRepository;
 import com.diningreview.DiningReviewAPI.repository.UserRepository;
 import com.diningreview.DiningReviewAPI.repository.RestaurantRepository;
 
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+import org.springframework.http.HttpStatus;
+import org.springframework.util.ObjectUtils;
+
 import java.util.Optional;
 
+@RequestMapping("/reviews")
+@RestController
 public class ReviewController {
+    private final ReviewRepository reviewRepository;
+    private final UserRepository userRepository;
+    private final RestaurantRepository restaurantRepository;
 
+    public ReviewController(ReviewRepository reviewRepository, UserRepository userRepository, RestaurantRepository restaurantRepository) {
+        this.reviewRepository = reviewRepository;
+        this.userRepository = userRepository;
+        this.restaurantRepository = restaurantRepository;
+    }
 }
--- src/main/java/com/diningreview/DiningReviewAPI/model/Rating.java
+++
@@ -1,5 +0,0 @@
-package com.diningreview.DiningReviewAPI.model;
-
-public enum Rating {
-
-}
