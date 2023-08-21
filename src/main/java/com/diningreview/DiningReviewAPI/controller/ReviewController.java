89b395471dcd137d37771a7455faf6eae3b096d3
Andrew Tran <tranandrew@gmail.com>
2023-08-21 12:28:26 -0500

    add POJO queries findReviewByRestaurantAndStatus() and FindReviewsByStatus() to Review.java

---
+++ src/main/java/com/diningreview/DiningReviewAPI/controller/ReviewController.java
@@ -0,0 +1,15 @@
+package com.diningreview.DiningReviewAPI.controller;
+import com.diningreview.DiningReviewAPI.model.Review;
+import com.diningreview.DiningReviewAPI.model.ReviewStatus;
+import com.diningreview.DiningReviewAPI.model.User;
+import com.diningreview.DiningReviewAPI.model.Restaurant;
+
+import com.diningreview.DiningReviewAPI.repository.ReviewRepository;
+import com.diningreview.DiningReviewAPI.repository.UserRepository;
+import com.diningreview.DiningReviewAPI.repository.RestaurantRepository;
+
+import java.util.Optional;
+
+public class ReviewController {
+
+}
