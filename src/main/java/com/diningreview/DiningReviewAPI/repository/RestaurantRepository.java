3b3a25fd484c32a5712bb6acab960f15e794526e
Andrew Tran <tranandrew@gmail.com>
2023-09-08 12:19:24 -0500

    added ScoreNotNull to each query so that query does not return any restaurant with a null allergen score

--- src/main/java/com/diningreview/DiningReviewAPI/repository/RestaurantRepository.java
+++ src/main/java/com/diningreview/DiningReviewAPI/repository/RestaurantRepository.java
@@ -4,8 +4,8 @@ import org.springframework.data.repository.CrudRepository;
 import java.util.ArrayList;
 
 public interface RestaurantRepository extends CrudRepository<Restaurant, Integer> {
-    ArrayList<Restaurant> findRestaurantByZipCodeOrderByPeanutScoreDesc(String zipCode);
-    ArrayList<Restaurant> findRestaurantByZipCodeOrderEggScoreDesc(String zipCode);
-    ArrayList<Restaurant> findRestaurantByZipCodeOrderDairyScoreDesc(String zipCode);
+    ArrayList<Restaurant> findRestaurantByZipCodeAndPeanutScoreNotNullOrderByPeanutScoreDesc(String zipCode);
+    ArrayList<Restaurant> findRestaurantByZipCodeAndEggScoreNotNullOrderEggScoreDesc(String zipCode);
+    ArrayList<Restaurant> findRestaurantByZipCodeAndDairyScoreNotNullOrderDairyScoreDesc(String zipCode);
 }
 
