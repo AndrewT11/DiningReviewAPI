c660701f003a572299f912b0c5885f165756cb19
Andrew Tran <tranandrew@gmail.com>
2023-09-08 08:28:39 -0500

    Lombok @Getter, @Setter, @AlArgsConstructor added to Restaurant.java model

--- src/main/java/com/diningreview/DiningReviewAPI/model/Restaurant.java
+++ src/main/java/com/diningreview/DiningReviewAPI/model/Restaurant.java
@@ -1,4 +1,9 @@
 package com.diningreview.DiningReviewAPI.model;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.GeneratedValue;
 import javax.persistence.Entity;
 import javax.persistence.GenerationType;
@@ -6,6 +11,9 @@ import javax.persistence.Id;
 import javax.persistence.Column;
 
 @Entity
+@Getter
+@Setter
+@AllArgsConstructor
 public class Restaurant {
     @Id
     @GeneratedValue
