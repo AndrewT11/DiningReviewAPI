23ce05d8f8d7396cf7e9aeaa455af31c002354c3
Andrew Tran <tranandrew@gmail.com>
2023-09-08 10:21:48 -0500

    add AdminReview model. Action to either accept or reject a dining review

---
+++ src/main/java/com/diningreview/DiningReviewAPI/model/AdminReview.java
@@ -0,0 +1,10 @@
+package com.diningreview.DiningReviewAPI.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AdminReview {
+    private Boolean reviewStatus;
+}
